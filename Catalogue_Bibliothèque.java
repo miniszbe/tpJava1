4c3b5118ffbe4b854bb361fcc6371bb553769251
miniszbe <miniszbe@users.noreply.github.com>
2016-10-06 09:39:31 -0400

    Classe Catalogue

---
+++ Catalogue_Bibliothèque.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Catalogue_Bibliothèque here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Catalogue_Bibliothèque
+{
+    public Catalogue_Bibliothèque(int p_AdresseCatalogue)
+    {
+        this.AdresseCatalogue = p_AdresseCatalogue;
+    }
+    
+    int getAdresseCatalogue()
+    {
+        return this.AdresseCatalogue;
+    }
+}
