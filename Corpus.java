4279c32bc13ddce975b8d0a983acd35ecb0d7d5c
miniszbe <miniszbe@users.noreply.github.com>
2016-10-06 09:24:33 -0400

    CorpuClass

---
+++ Corpus.java
@@ -0,0 +1,29 @@
+
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+public class Corpus
+{
+    // instance variables - replace the example below with your own
+
+    public Corpus(String p_Statut, int p_Position)
+    {
+        this.Statut = p_Statut;
+        this.Position = p_Position;
+    }
+    
+    String getStatut()
+    {
+        return this.Statut;
+    }
+    
+    int getPosition()
+    {
+        return this.Position;
+    }
+
+
+}
