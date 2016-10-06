b157ba212262e2296a151ebd0755a80a4165de29
miniszbe <miniszbe1@gmail.com>
2016-10-06 09:02:43 -0400

    ClassSiteWeb
    Premiere classe, encore a modifier pour le bon fonctionnement du programme a la fin.

---
+++ SiteWeb.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class SiteWeb here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SiteWeb
+{
+
+    public SiteWeb(string p_adressWeb)
+    {
+        this.Adresse = p_adressWeb;
+    }
+    
+    String getAdresse()
+    {
+        return this.Adresse;
+    }
+}
